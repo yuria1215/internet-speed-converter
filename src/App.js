ac3554c9e302825d4b74770bba57e2ed43104607
yuria1215 <takaii666@gmail.com>
2023-04-09 17:57:45 +0800

    計數器

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 // STEP 1：匯入 useState 方法
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import UnitControl from './components/UnitControl';
-import CardFooter from './components/CardFooter';
-import UnitConverter from './components/UnitConverter';
+import UnitControl from "./components/UnitControl";
+import CardFooter from "./components/CardFooter";
+import UnitConverter from "./components/UnitConverter";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 library.add(fab, fas, far);
-
-
-
-
 
 function App() {
   //定義state，取得預設值為0的inputValue和修改狀態的setInputValue方法
@@ -25,27 +21,52 @@ function App() {
 
   //定義事件處理器
   const handleInputChange = (e) => {
-    console.log(e)
+    console.log(e);
     const { value } = e.target;
     setInputValue(value);
-  }
+  };
+
+  // 計數器
+  // let count = 0;
+  const [count, setCount] = useState(0);
+
+  const click = (direction) => {
+    if (direction === "-") {
+      setCount(count - 1);
+    } else {
+      setCount(count + 1);
+    }
+
+    console.log("count", count);
+  };
 
   return (
-    <div className="container">
-      <div className="card-header">Network Speed Converter</div>
-      <div className="card-body">
+    <div>
+      <div className="container">
+        <div className="card-header">Network Speed Converter</div>
+        <div className="card-body">
+          <UnitControl />
 
-        <UnitControl />
+          <UnitConverter
+            inputValueKey={inputValue}
+            handleInputChangeKey={handleInputChange}
+          />
+        </div>
 
-        <UnitConverter
-          inputValueKey={inputValue}
-          handleInputChangeKey={handleInputChange} />
-
+        {/* STEP 1 : 把想要傳入 CardFooter 的資料透過 key={value} 的方式傳入 */}
+        <CardFooter inputValueKey={inputValue} />
       </div>
 
-      {/* STEP 1 : 把想要傳入 CardFooter 的資料透過 key={value} 的方式傳入 */}
-      <CardFooter inputValueKey={inputValue} />
-
+      {/* 計數器 */}
+      <div className="container countNumber" style={{ marginTop: "30px" }}>
+        <div className="chevron chevron-down" onClick={() => click("-")}>
+          -
+        </div>
+        <div className="number">{count}</div>
+        <div className="chevron chevron-up" onClick={() => click("+")}>
+          +
+        </div>
+      </div>
     </div>
   );
 }
